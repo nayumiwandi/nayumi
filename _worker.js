d4b3a61836e394b43d761546c74a870e67bcdfe5
muria-pixel <madjidhariadi5@gmail.com>
2025-05-10 00:14:36 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,8 +3,8 @@ import { connect } from "cloudflare:sockets";
 // import { Buffer } from "node:buffer";
 
 // Variables
-const rootDomain = "layanan-yumi.workers.dev"; // Ganti dengan domain utama kalian
-const serviceName = "nayumi"; // Ganti dengan nama workers kalian
+const rootDomain = "muriavpn.workers.dev/"; // Ganti dengan domain utama kalian
+const serviceName = "kopi"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
